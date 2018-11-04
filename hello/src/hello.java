9a97c9229287c69b74c24518f95c00182ec1e9e5
Hiusz <787217653@qq.com>
2018-11-04 15:12:54 +0800

    helloworld

--- hello/src/hello.java
+++ hello/src/hello.java
@@ -1,5 +1,12 @@
 public class hello {
-    public static void main(String arg[]){
-        System.out.print("helloworld");
+    public static void main(String arg[])
+    {
+        int a=5275;
+        int b=9;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.print(a);
+        System.out.print(b);
     }
 }
